c701755016b81e37fb323e3ef0cfc84295600ee8
Mike LeDoux <michael.ledoux@accenture.com>
2019-02-19 10:21:50 -0500

    Changes in progress for 'Our Work' animations...

--- src/scripts/work.js
+++ src/scripts/work.js
@@ -1,39 +1,83 @@
 import anime from 'animejs';
 import ScrollMagic from 'scrollmagic';
 
+let $currSlide = null;
+let $nextSlide = null;
+
+
 const animationIn = anime
-.timeline({ loop: false })
+.timeline({ 
+    loop: false,
+    autoplay: false
+})
 .add(
-    {
-      targets: '.inner-card .caption, .inner-card .dp-card-title, .inner-card .body-text-2',
-      translateX: [-60, 0],
-      opacity: [0, 1],
-      easing: 'easeInOutSine',
-      duration: 1000,
-      delay: 400
-    })
-    .add(
-    {
-        targets: '.inner-card .buttons',
-        translateX: [-80, 0],
-        opacity: [0, 1],
-        easing: 'easeInOutSine',
-        duration: 1000,
-    }, '-=500')
-    .add(
-    {
-        targets: '.feat-img',
-        translateY: [100, 0],
-        opacity: [0, 1],
-        easing: 'easeInOutSine',
-        duration: 1000,
-    }, '-=500');
+{
+    targets: '.card.active .caption, .card.active .dp-card-title, .card.active .body-text-2',
+    translateX: [-60, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+    delay: 400
+})
+.add(
+{
+    targets: '.card.active .buttons',
+    translateX: [-80, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+}, '-=500')
+.add(
+{
+    targets: '.card.active .feat-img',
+    translateY: [100, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+}, '-=500');
+    
+const animationNext = anime
+.timeline({ 
+    loop: false,
+    autoplay: false
+})
+.add(
+{
+    targets: '.card.active .caption, .card.active .dp-card-title, .card.active .body-text-2',
+    translateX: [-60, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+    delay: 400,
+})
+.add(
+{
+    targets: '.card.active .buttons',
+    translateX: [-80, 0],
+    opacity: [0, 1],
+    easing: 'easeInOutSine',
+    duration: 1000,
+}, '-=500')
+.add(
+{
+targets: '.card.active .feat-img',
+translateY: [100, 0],
+opacity: [0, 1],
+easing: 'easeInOutSine',
+duration: 1000,
+begin: () => {
+    $nextSlide.addClass('active');
+}
+}, '-=500');
     
 const animationOut = anime
-.timeline({ loop: false })
+.timeline({ 
+    loop: false,
+    autoplay: false
+})
 .add(
     {
-        targets: '.feat-img',
+        targets: '.card.active .feat-img',
         translateY: [0, 200],
         opacity: [1, 0],
         easing: 'easeInOutSine',
@@ -42,20 +86,22 @@ const animationOut = anime
     })
 .add(
     {
-        targets: '.inner-card .caption, .inner-card .dp-card-title, .inner-card .body-text-2',
+        targets: '.card.active .caption, .card.active  .dp-card-title, .card.active  .body-text-2',
         opacity: [1, 0],
         easing: 'easeInOutSine',
         duration: 1000
     }, '-=800')
 .add(
 {
-    targets: '.inner-card .buttons',
+    targets: '.card.active .buttons',
     opacity: [1, 0],
     easing: 'easeInOutSine',
     duration: 1000,
+    complete: () => {
+        $currSlide.removeClass('active');        
+    }
 }, '-=800');
             
-
 const controller = new ScrollMagic.Controller();
 
 // Triggers on 'top of div'
@@ -68,7 +114,7 @@ const workScene1 = new ScrollMagic.Scene({
 .on('progress', event => {
     animationIn.play();
 });
-
+  
 const workScene2 = new ScrollMagic.Scene({
     triggerElement: '.upward-work-trigger-element',
     duration: 0,
@@ -79,9 +125,12 @@ const workScene2 = new ScrollMagic.Scene({
     //animationOut.play();
 });
   
-$('.next').on('click', ()=> {
+$('.next').on('click', (el)=> {
+    $currSlide = $(el.currentTarget).parent().find('.active')
+    $nextSlide = $currSlide.next('.card');
     animationOut.play();
+    animationNext.play();
 });
 $('.prev').on('click', ()=> {
-    animationIn.play();
+    //animationIn.play();
 });
