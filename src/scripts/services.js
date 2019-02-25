7ff414ddcb249191d43484823cd0ad3ff6c8b59f
Davis, Emily <emily.davis@accenture.com>
2019-02-25 16:56:51 -0700

    Adds style changes on click events (static—no animations yet)

--- src/scripts/services.js
+++ src/scripts/services.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
 
+  const $circles = $('.timeline .circle');
+  const $lineSegments = $('.timeline .line-segment');
+  const $labels = $('.timeline-labels .timeline-label');
+
   function selectTimelineStop(stopNumber) {
-    console.log('thank you for calling moviefone. you have selected ', stopNumber);
+    $lineSegments.map(function(index, segment) {
+      if (index <= stopNumber) {
+        $(segment).removeClass('future').addClass('past');
+      } else {
+        $(segment).removeClass('past').addClass('future');
+      }
+    });
+    $circles.map(function(index, circle) {
+      if (index < stopNumber) {
+        $(circle).removeClass('future').removeClass('current').addClass('past');
+      } else if (index === stopNumber) {
+        $(circle).removeClass('future').removeClass('past').addClass('current');
+      } else {
+        $(circle).removeClass('current').removeClass('past').addClass('future');
+      }
+    });
+    const $selectedLabel = $labels.eq(stopNumber);
+    $labels.removeClass('current');
+    $selectedLabel.addClass('current');
   }
 
   $('.timeline .circle').on('click', function(event) {
-    const stopNumber = $('.timeline .circle').index($(event.target));
+    const stopNumber = $circles.index($(event.target));
     if (stopNumber > -1) {      
       selectTimelineStop(stopNumber);
     }
   });
 
   $('.timeline-labels .timeline-label').on('click', function() {
-    const stopNumber = $('.timeline-labels .timeline-label').index($(event.target));
+    const stopNumber = $labels.index($(event.target));
     if (stopNumber > -1) {
       selectTimelineStop(stopNumber);
     }
