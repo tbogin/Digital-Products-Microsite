b70744eb6017088e520f22ab0f4c7fefdda9b1cb
Davis, Emily <emily.davis@accenture.com>
2019-02-27 16:12:27 -0700

    Fixes issue preventing some assets from being copied into the build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -20,7 +20,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['build'], { root: Path.resolve(__dirname, '..') }),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+      { from: Path.resolve(__dirname, '../public'), to: 'public' },
+      { from: Path.resolve(__dirname, '../src/assets'), to: 'src/assets' }
     ]),
     new HtmlWebpackPlugin({
       template: Path.resolve(__dirname, '../src/index.html')
