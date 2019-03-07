10326dd96ef4cd07618a2a72eb08a1ba2eeed13c
ted.bogin <ted.bogin@accenture.com>
2019-03-07 10:20:51 -0500

    Removed superfluous comments and code

--- src/scripts/work-slides.js
+++ src/scripts/work-slides.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   const $buttonContainer = $('#work-btn-container');
   const $slickContainer = $('.slide-wrap');
 
-  //Dynamically populate a tab button for each slide
+  //Dynamically populate a tab button for each slide. No tabs if there is only one slide
   $.each($slides, (i, el) => {
     if($slides.length > 1) {
       $($buttonContainer).append(
@@ -19,15 +19,6 @@ $(document).ready(function() {
     }
   });
   $('.work-btn:first').addClass('active');
-
-  // /* Desktop (not-mobile) constants and utility methods */
-  const $workCardSubcontainer = $('.slide-wrap');
-
-  function getDevButtonOffset() {
-    return $('.work-main-text-col').outerHeight() - ($('#button-work-1').outerHeight() + 60);
-  }
-  /* End desktop (not-mobile) constants and utility methods */
-
 
   /* Mobile utility methods */
   function getFirstDevCardIndex() {
@@ -58,7 +49,7 @@ $(document).ready(function() {
 
   // ScrollMagic scene controlling initial animation of text, buttons, & cards
   const initialAnimationScene = new ScrollMagic.Scene({
-    triggerElement: '.slide-wrap', //may need to be one element deeper
+    triggerElement: '.slide-wrap',
     duration: 0,
     triggerHook: 0.65
   }).addTo(capabilitiesController)
@@ -79,36 +70,5 @@ $(document).ready(function() {
       this.animateTiles();
     }
   };
-
-  /* 
-     getTriggerHook function, to be used with ScrollMagic buttonSelectionScene (below).
-     triggerHook must be a number between 0 and 1 representing a ratio of the vertical position of the trigger to the height of the viewport.
-     the calculations below set the triggerHook to be level with the top of the "Development Capabilities" button 
-  */
-  function getTriggerHook() {
-    return getDevButtonOffset() / $(window).outerHeight();
-  }
-
-  // ScrollMagic scene to toggle button states (i.e., which button appears to be active) based on scroll position  - Desktop (not-mobile) only
-  const designCardContainerHeight = $workCardSubcontainer.outerHeight();
-  const buttonSelectionScene = new ScrollMagic.Scene({
-    triggerElement: '.slide-wrap',
-    offset: -40,    // this aligns the "start" of the scene with the top of the first right-hand card in the .capabilities-design-card-container
-    duration: designCardContainerHeight + 100,    // this aligns the "end" of the scene with the top of the first left-hand card in the .capabilities-dev-card-container
-    triggerHook: getTriggerHook()
-  }).addTo(capabilitiesController)
-    .on('enter', function() {
-      // if (isNotMobile()) {
-      //   highlightSelectedButton('design');
-      // }
-    }
-  ).on('leave', function(event) {}
-  );
-
-  // Reset triggerHook dynamically when the scene "shifts", for example, when the window is resized
-  buttonSelectionScene.on('shift', function(event) {
-    buttonSelectionScene.triggerHook(getTriggerHook());
-  });
   /* End ScrollMagic */
-
 });
