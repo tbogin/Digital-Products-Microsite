d64ef414743b8a999b2554984264e290d49c00f0
ted.bogin <ted.bogin@accenture.com>
2019-03-07 16:51:06 -0500

    Implemented slider for Contact section; added images

--- src/scripts/contacts.js
+++ src/scripts/contacts.js
@@ -1,13 +1,8 @@
 
 $(document).ready(function(){
     const $slickContainer = $('.contact-card-container');
+
     $slickContainer.slick({
-      // arrows: true,
-      // centerMode: false,
-      // infinite: false,
-      // slidesToShow: 4,
-      // slidesToScroll: 1,
-      // variableWidth: true,
       prevArrow: $('.prev-arrow-custom'), 
       nextArrow: $('.next-arrow-custom'),           
       centerMode: false,    
@@ -16,14 +11,4 @@ $(document).ready(function(){
       slidesToScroll: 1,              
       variableWidth: true,
     });
-
-    // arrows: false,            
-    // centerMode: true,    
-    // infinite: false,
-    // slidesToShow: 1,         
-    // slidesToScroll: 1,              
-    // variableWidth: true, 
-
-    // $slickContainer.slick();
-
   });
