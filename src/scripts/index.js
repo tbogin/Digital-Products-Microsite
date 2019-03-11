4fb00fb6863587b2d086f3b50095bcdb6a48a414
ted.bogin <ted.bogin@accenture.com>
2019-03-11 15:27:47 -0400

    Added desktop vertical lines

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,6 +11,7 @@ import '../styles/_process.scss';
 import '../styles/_cards.scss';
 import '../styles/_services.scss';
 import '../styles/_contact.scss';
+import '../styles/_vertical-lines.scss';
 
 // JS imports
 import './nav.js';
