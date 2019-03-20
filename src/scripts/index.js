8d2b8b846ad8bc219597b3a6c45868416ce66478
ted.bogin <ted.bogin@accenture.com>
2019-03-20 17:01:19 -0400

    Updated styles

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -21,7 +21,6 @@ import './capabilities.js';
 import './work.js';
 import './constants.js';
 import './services.js';
-import './services-pager.js';
 import './timelineState.js';
 import './contacts.js';
 import './scroll.js';
